dbfa3b5f8e4ea976707b75b5e2121ffa7919546b
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-05-25 14:30:15 -0700

    Update Main.java

--- src/BeepBeepCipher/Main.java
+++ src/BeepBeepCipher/Main.java
@@ -1,7 +1,28 @@
 package BeepBeepCipher;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello! Welcome to the Cipher Area! Please choose an option from below!");
+        System.out.println("Hello! Welcome to our encryption project! Choose an option below!");
+        System.out.println("1) ASCII");
+        System.out.println("2) Random Swap");
+        System.out.println("3) Binary");
+        System.out.println("4) QWERTY Swap");
+        Scanner s = new Scanner(System.in);
+        int choice = s.nextInt();
+        switch(choice){
+            case 1:
+                ASCII.run();
+                break;
+            case 2:
+                RandomSwap.run();
+                break;
+            case 3:
+
+            case 4:
+
+        }
     }
+
 }
