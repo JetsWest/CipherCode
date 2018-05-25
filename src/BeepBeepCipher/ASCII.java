051a7a88cd01abf467943ea511d5bf6cd4c71bc7
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-05-25 14:25:11 -0700

    Update ASCII.java

--- src/BeepBeepCipher/ASCII.java
+++ src/BeepBeepCipher/ASCII.java
@@ -1,5 +1,35 @@
 package BeepBeepCipher;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class ASCII {
-    public static void main(String[] args){}
+    private static ArrayList<Integer> a = new ArrayList<>();
+    
+    public static void swap(ArrayList<String> arr){
+        for (int i = 0; i < arr.size(); i++){
+            a.add(i, (int) arr.get(i).charAt(0));
+        }
+        for (int j = 0; j < a.size(); j++){
+            System.out.print(a.get(j));
+        }
+    }
+    public static void main(String[] args) {
+        ArrayList<List<String>> listOfList = new ArrayList<List<String>>(0);
+        System.out.println("Hello! Please input the string you would want changed below!");
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        if (input.equals("")) {
+            System.out.println("You must actually type something goofy head!");
+        } else {
+            List<String> list = Arrays.asList(input.split(""));
+            ArrayList<String> n = new ArrayList<>(list);
+            listOfList.add(list);
+            swap(n);
+        }
+    }
 }
+
