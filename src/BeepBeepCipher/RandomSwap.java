140d42eb26dc69e9d5d538fe6906bb7811c41e05
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-05-25 13:46:03 -0700

    Update RandomSwap.java

--- src/BeepBeepCipher/RandomSwap.java
+++ src/BeepBeepCipher/RandomSwap.java
@@ -9,6 +9,14 @@ public class RandomSwap {
             Collections.swap(arr, i, (int) (Math.random() * i));
         }
     }
+    public static void decode(ArrayList<List<String>> arr){
+        System.out.println("Which item would you like to decode from RANDOM SWAP? Input below!");
+        Scanner s = new Scanner(System.in);
+        int j = s.nextInt();
+        for (int i = 0; i < arr.get(j-1).size(); i++){
+            System.out.print(arr.get(j-1).get(i));
+        }
+    }
     public static void main(String[] args){
         ArrayList<List<String>> listOfList = new ArrayList<List<String>>(0);
         System.out.println("Hello! Please input the string you would want changed below!");
@@ -26,7 +34,7 @@ public class RandomSwap {
                 str += string;
             }
             System.out.println(str);
-            System.out.println(Arrays.asList(listOfList));
+            decode(listOfList);
         }
     }
 }
