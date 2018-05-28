89d82380a0b91c6392590bc4c7715ba4ee793aa1
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-05-28 11:49:44 -0700

    Update FileOutput.java

--- src/BeepBeepCipher/FileOutput.java
+++ src/BeepBeepCipher/FileOutput.java
@@ -1,5 +1,16 @@
 package BeepBeepCipher;
+import java.io.*;
 
 public class FileOutput {
+    public static void print(String str) throws IOException {
+        File f = new File("Output.txt");
+        FileOutputStream fos = new FileOutputStream(f);
+        PrintWriter pw = new PrintWriter(fos);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        pw.write(str);
+        pw.flush();
+        fos.close();
+        pw.close();
+    }
 }
