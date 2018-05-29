91290283a0f8562e0352461f98268d5781de3f19
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-05-29 08:43:11 -0700

    Update Main.java

--- src/BeepBeepCipher/Main.java
+++ src/BeepBeepCipher/Main.java
@@ -1,11 +1,10 @@
-package BeepBeepCipher;
-
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello! Welcome to our encryption project! Choose an option below!");
-        System.out.println("1) ASCII");
+        System.out.println("1) Base64");
         System.out.println("2) Random Swap");
         System.out.println("3) Binary");
         System.out.println("4) QWERTY Swap");
@@ -13,7 +12,7 @@ public class Main {
         int choice = s.nextInt();
         switch(choice){
             case 1:
-                ASCII.run();
+                Base64Decode.run();
                 break;
             case 2:
                 RandomSwap.run();
