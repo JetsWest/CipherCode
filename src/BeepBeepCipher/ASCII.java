4ceb78a5a2998ee31bd194abc3df36c9f0ac74bb
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-05-31 08:14:09 -0700

    Update ASCII.java

--- src/BeepBeepCipher/ASCII.java
+++ src/BeepBeepCipher/ASCII.java
@@ -1,36 +1,43 @@
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.util.Base64;
 import java.util.Scanner;
 
 public class Base64Decode {
+    private static FileOutputStream fos;
+    private static PrintWriter pw;
+    private static String encodedString;
     public static void decode(String encoded) throws UnsupportedEncodingException {
-        byte[] encodedString = Base64.getDecoder().decode(encoded);
-        System.out.println(new String(encodedString, "UTF-8"));
+        byte[] encoder = Base64.getDecoder().decode(encoded);
+        System.out.println(new String(encoder, "UTF-8"));
     }
     public static void encode(String str){
-        String encodedString = Base64.getEncoder().encodeToString(str.getBytes());
+        encodedString = Base64.getEncoder().encodeToString(str.getBytes());
         System.out.println(encodedString);
     }
-    public static void run() throws UnsupportedEncodingException {
+    public static void run() throws IOException {
         System.out.println("Welcome! Would you like to decode or encode today?");
         System.out.println("1) Decode");
         System.out.println("2) Encode");
         Scanner s = new Scanner(System.in);
-        int x = s.nextInt();
-        if (x == 1){
+        String x = s.nextLine();
+        if (x.equals("1")){
             System.out.println("Input a string - already in Base64 - below!");
             String c = s.nextLine();
             decode(c);
-        }else if (x == 2){
+            File f = new File(c + ".txt");
+            System.out.println("You can find this file in your IdeaProjects folder");
+            FileOutput.print(encodedString, f);
+        }else if (x.equals("2")){
             System.out.println("Input a string below!");
             String p = s.nextLine();
             encode(p);
+            File f = new File(p + ".txt");
+            System.out.println("You can find this file in your IdeaProjects folder");
+            FileOutput.print(encodedString, f);
         }else{
             System.out.println("THOT BE NIMBLE, THOT BE QUICK, BUT THOT DOES NOT HOW HOW TO USE BASE64 CLICK");
         }
     }
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         run();
     }
-}
