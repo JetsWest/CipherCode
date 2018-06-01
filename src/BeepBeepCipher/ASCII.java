84808cab72c22b56d4419326198de17dddf2c84f
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-06-01 08:38:35 -0700

    Update ASCII.java

--- src/BeepBeepCipher/ASCII.java
+++ src/BeepBeepCipher/ASCII.java
@@ -1,3 +1,4 @@
+package cipher;
 import java.io.*;
 import java.util.Base64;
 import java.util.Scanner;
@@ -22,11 +23,10 @@ public class Base64Decode {
         String x = s.nextLine();
         if (x.equals("1")){
             System.out.println("Input a string - already in Base64 - below!");
+            System.out.println("It will not output to a file. Why would you need to remember the decoded?");
+            System.out.println("Just come back and do it again if ya need to :D");
             String c = s.nextLine();
             decode(c);
-            File f = new File(c + ".txt");
-            System.out.println("You can find this file in your IdeaProjects folder");
-            FileOutput.print(encodedString, f);
         }else if (x.equals("2")){
             System.out.println("Input a string below!");
             String p = s.nextLine();
@@ -36,8 +36,19 @@ public class Base64Decode {
             FileOutput.print(encodedString, f);
         }else{
             System.out.println("THOT BE NIMBLE, THOT BE QUICK, BUT THOT DOES NOT HOW HOW TO USE BASE64 CLICK");
+            run();
+        }
+        System.out.println("Run again? 1) Yes 2) No");
+        int y = s.nextInt();
+        if (y == 1){
+            Main.run();
+        }else if (y == 2){
+            System.exit(0);
+        }else{
+            System.out.println("REEEEEEE! You do not get to run again after your goofy actions have killed thee.");
         }
     }
     public static void main(String[] args) throws IOException {
         run();
     }
+}
